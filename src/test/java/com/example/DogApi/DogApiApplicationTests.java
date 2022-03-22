cd000b7e0bd753a8c43ee6a34bd8d27c629a8698
sinamhbb <sina.mhbbi@gmail.com>
2022-03-22 09:41:52 +0200

    Application Boot Strap

---
+++ src/test/java/com/example/DogApi/DogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DogApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
